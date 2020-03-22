3e7cac7256b37ce75f4bfc18d626fd862bcd18c5
Rustam Kashapov <hardtechnik91@gmail.com>
2020-03-22 12:32:42 +0300

    initial

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name='Chunked',
+      version='0.0.1',
+      description='This is a simple program that encodes an input stream '
+                  'using HTTP "chunked" encoding',
+      author='Rustam Kashapov',
+      author_email='hardtechnik91@gmail.com',
+      scripts=['chunked'])
